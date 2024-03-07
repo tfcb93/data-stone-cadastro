52303a87056bcefb3e94021b2c24053e16114e65
Thiago Borges <thiago@tfcb93.com>
2024-03-06 22:47:57 -0300

    Adds vuetify to the project

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,27 @@ import { createPinia } from 'pinia'
 import './style.scss'
 import App from './App.vue'
 
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+
 const pinia = createPinia();
 const app = createApp(App);
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    }
+});
+
 app.use(pinia);
+app.use(vuetify)
 app.mount('#app');
